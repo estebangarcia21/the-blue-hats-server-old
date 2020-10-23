03d3fcf1c98078f820517a110d5acc646b0a7885
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-23 15:12:44 -0700

    Restored the Noob Rank

---
+++ Blue Permissions/src/me/stevemmmmm/permissions/ranks/NoobRank.java
@@ -0,0 +1,55 @@
+package me.stevemmmmm.permissions.ranks;
+
+import me.stevemmmmm.permissions.core.Rank;
+import org.bukkit.ChatColor;
+
+import java.util.HashMap;
+
+/*
+ * Copyright (c) 2020. Created by Stevemmmmm.
+ */
+
+public class NoobRank extends Rank {
+
+    @Override
+    public String getName() {
+        return "Noob";
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public HashMap<String, Boolean> getPermissions() {
+        return new HashMap<String, Boolean>() {
+            private static final long serialVersionUID = 1L;
+            {
+                put("minecraft.command.me", false);
+                put("bukkit.command.help", false);
+                put("bukkit.command.plugins", false);
+            }
+        };
+    }
+
+    @Override
+    public String getPrefix() {
+        return ChatColor.GRAY + "[Noob]";
+    }
+
+    @Override
+    public ChatColor getNameColor() {
+        return ChatColor.GRAY;
+    }
+
+    @Override
+    public ChatColor getChatColor() {
+        return ChatColor.GRAY;
+    }
+
+    @Override
+    public RankType getRankType() {
+        return RankType.PLAYER;
+    }
+}
